2715dffba4c43ebadba6cf5e1cc3921b1f3f4704
Kurmanbek Nurmatov <nokobo.prog@gmail.com>
2020-06-03 17:58:44 +0300

    design the data management page

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/management', function () {
+    return view('management.index');
+});
