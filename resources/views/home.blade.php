1d5b3b9d6bec6d50c347cdb60dade717ffcdc206
Kurmanbek Nurmatov <nokobo.prog@gmail.com>
2020-06-03 14:22:48 +0300

    design the main function page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,31 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    You are logged in!
+                    <div class="row text-center">
+                        <div class="col-sm-4">
+                            <a href="/management">
+                                <h4>Management</h4>
+                                <img width="50px" src="{{asset('images/management.svg')}}" alt="">
+                            </a>
+                        </div>
+                        <div class="col-sm-4">
+                            <a href="/cashier">
+                                <h4>Cashier</h4>
+                                <img width="50px" src="{{asset('images/cashier.svg')}}" alt="">
+                            </a>
+                        </div>
+                        <div class="col-sm-4">
+                            <a href="/report">
+                                <h4>Report</h4>
+                                <img width="50px" src="{{asset('images/report.svg')}}" alt="">
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
